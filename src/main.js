4a350fd46a9cbb2f83ea99180eb68e728d8ee630
Cansiny0320 <1136843910@qq.com>
2020-08-24 18:10:40 +0800

    feat: init

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import './assets/style/reset.scss';
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
-  render: h => h(App)
-}).$mount("#app");
+  render: h => h(App),
+}).$mount('#app');
