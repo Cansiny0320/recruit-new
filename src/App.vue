6ed1eca5b3d024fb2c4bcc9619a438db25ed80f2
Qixing Lin <3077006505@qq.com>
2020-08-26 15:28:42 +0800

    feat:新增打字特效组件

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <template>
   <div id="app">
-    <router-view />
+    <!-- <router-view /> -->
+    <type :typetxt="loadingTxt1"></type>
   </div>
 </template>
 
 <script>
-export default {}
+import type from "./components/type"
+export default {
+  components: {
+    type
+  },
+  data () {
+    return {
+      loadingTxt1: "20年前，互联网时代的大幕徐徐拉开，诞生了一个又一个神话"
+    }
+  }
+}
 </script>
 
 <style lang="scss">
---
+++ src/components/type.vue
@@ -0,0 +1,47 @@
+<!-- 打字特效 -->
+<template>
+  <div class="type">
+    {{ txt2 }}
+    <audio ref="audio" class="audio" autoplay loop src="../assets/bgm/txtType.mp3"></audio>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    typetxt: String,
+  },
+  components: {},
+  data () {
+    return {
+      txt1: [],
+      txt2: "",
+    };
+  },
+  mounted () {
+    //打字特效
+    let txt = this.typetxt.split("");
+    //this.$refs.audio.play();
+    let txtplay = setInterval(() => {
+      this.txt1.push(txt.shift());
+      this.txt2 = String(this.txt1).replace(/,/g, "");
+      if (txt == "") {
+        clearInterval(txtplay);
+        this.$refs.audio.pause();
+        // console.log("暂停");
+      }
+    }, 100);
+  },
+};
+</script>
+<style lang="scss" scoped>
+@font-face {
+  font-family: 'AliHYAiHei';
+  src: url('../assets/font-style/AliHYAiHei.ttf');
+}
+
+.type {
+  font-family: 'AliHYAiHei';
+  text-align: left;
+}
+</style>
