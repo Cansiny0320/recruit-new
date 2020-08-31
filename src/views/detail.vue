ca18024021934d04efac36d2d2057cfda155ad36
Qixing Lin <3077006505@qq.com>
2020-08-31 15:35:28 +0800

    fix:修改动画函数

--- src/views/detail.vue
+++ src/views/detail.vue
@@ -61,12 +61,12 @@ export default {
         a = obj.lon < 180 ? obj.lon : obj.lon - 360;
         b = obj.lat;
 
-        a = a > 0 ? a > 360 ? 360 : a : a < -360 ? -360 : a;
+        a = a > 0 ? a > 50 ? 50 : a : a < -50 ? -50 : a;
         b = b > 0 ? b > 50 ? 50 : b : b < -50 ? -50 : b;
         //console.log(obj.a, obj.b, obj.g);
         //img[0].style.transform = `translate3d(${a}px,${b}px,${0}px)`
 
-        img[0].style.transform = `rotateX(${obj.b}deg) rotateY(${a}deg)`
+        img[0].style.transform = `rotateX(${b}deg) rotateY(${a}deg)`
 
         tip.innerHTML =
 
